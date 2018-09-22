64326ce29cf4d263f2b01919995613eddebda3f4
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-22 20:04:40 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,12 +3,16 @@ require 'pry'
 def reformat_languages(languages)
   new_hash = {}
 
-  languages.each do |style, style_value|
-    new_hash = {style_value: style }
-      binding.pry
-
+  languages.each do |type, lang_hash|
+    lang_hash.each do |lang_name, value|
+      value.each do |value_name, val|
+        if new_hash.key?(lang_name) == false
+          new_hash[lang_name] = {value_name: val, style:[type]}
+        else
+          new_hash[lang_name][:style] << type
+        end
+      end
+    end
   end
-
-
   new_hash
 end
