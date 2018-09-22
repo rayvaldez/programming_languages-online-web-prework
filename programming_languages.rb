249bcc4ac5dc05083e8460e7dd7e622431736bfd
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-22 19:55:22 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   new_hash = {}
 
   languages.each do |style, style_value|
-    new_hash[:style] = {style_value: "T" }
+    new_hash = {style_value: style }
       binding.pry
 
   end
