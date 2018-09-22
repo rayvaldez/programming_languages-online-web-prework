cc233843c36dbb4713e32bd47940fcc97d691013
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-22 19:42:20 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |style, style_value|
+    style_value.each do |k, v|
+
+      binding.pry
+    end
+  end
+
+
+  new_hash
 end
